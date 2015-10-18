da9ddd1db68a4c1e35cf261c02c000c6b73b9c72
Crutchcorn <crutchcorn@gmail.com>
2015-10-18 16:07:12 -0700

    Beginning work on making items user selectable

--- textapplauncher.py
+++ textapplauncher.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
+# Set up
 import os	
 import re
+import sys
 name=""
 pexec=""
-def addToFunction(location):
+
+# Add to CheckInput
+def addToCheckInput(location):
 	for filename in os.listdir(location):
 		appname=open(location+filename) 
 		for line in appname:
@@ -17,7 +21,7 @@ def addToFunction(location):
 		with open("checkinput.py", "a") as myfile:
 			myfile.write('	'+nospacename+'="'+name+'"\n')
 			myfile.write("	if userinput in "+nospacename+":\n")
-			myfile.write("		print(i)\n")
+			myfile.write("		print('Program number: ', i)\n")
 			myfile.write("		print('Program name: "+name+"')\n")
 			myfile.write("		print('Exec command: "+pexec+"')\n")
 			myfile.write("		i=i+1\n")
@@ -25,9 +29,28 @@ def addToFunction(location):
 			myfile.write("\n")
 		print(name)
 
+# This allows the second time of checkinput.py to be normal
+def generatechecknumbergen(location):
+	for filename in os.listdir(location):
+		appname=open(location+filename) 
+		for line in appname:
+			if "Name=" in line:
+				name=line.rstrip().replace('Name=','')
+				regex = re.compile('[^a-zA-Z]')
+				nospacename=regex.sub('', name)
+			if "Exec=" in line:
+					pexec=line.rstrip().replace('Exec=','')
+		# Create function
+		with open("checknumbergen.py", "a") as myfile:
+			myfile.write('	'+nospacename+'="'+name+'"\n')
+			myfile.write("	if userinput in "+nospacename+":\n")
+			myfile.write("		print('"+pexec+"')\n")
+			myfile.write(r"		print('\n')")
+			myfile.write("\n")
+
 home = os.path.expanduser("~")
-addToFunction("/usr/share/applications/")
-addToFunction(home+"/.local/share/applications/")
+addToCheckInput("/usr/share/applications/")
+addToCheckInput(home+"/.local/share/applications/")
 
 # Import function
 from checkinput import checkinput
@@ -42,7 +65,46 @@ print("These are some of the programs that you can use:\n")
 # Call Function
 checkinput(userinput)
 
-# Clear function
+# Generates checknumbergen.py
+with open("checknumbergen.py", "a") as myfile:
+	myfile.write('def checknumbergen(userinput):\n')
+generatechecknumbergen("/usr/share/applications/")
+generatechecknumbergen(home+"/.local/share/applications/")
+from checknumbergen import checknumbergen
+
+
+
+# Change checkinput.py to be output of checkinput(userinput)
+sys.stdout = open('checkinput.py', 'w')
+checknumbergen(userinput)
+sys.stdout.close()
+
+
+# Removes blank lines
+clean_lines = []
+with open("checkinput.py", "r") as f:
+    lines = f.readlines()
+    clean_lines = [l.strip() for l in lines if l.strip()]
+with open("checkinput.py", "w") as f:
+    f.writelines('\n'.join(clean_lines))
+
+
+#input("Go check your file corbin")
+
+# Make changes to checkinput.py to be similar to 
+'''
+from checkinput import checknumber
+
+userNumber=input("Pick a number to launch the program: ")
+
+if userNumber==2:
+		# Proper launching of bash command of pexec
+'''
+
+
+
+# Clear file to default
+open('checknumbergen.py', 'w').close()
 open('checkinput.py', 'w').close()
 with open("checkinput.py", "a") as myfile:
 	myfile.write("#!/usr/bin/python3\n")
